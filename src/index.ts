3b1092610971c23fcf9998c611ff86eff4cbf33c
SheepHan <williamcheng0928@gmail.com>
2019-08-13 07:14:46 +0800

    函数

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import './datatype3-1' //对象类型接口  2、可选属性
 import './datatype3-2' //对象类型接口  3、只读属性
 import './datatype3-3' //对象类型接口  4、可索引类型接口，当接口属性个数不确定时候使用
 import './datatype4'  //函数类型接口
-// import './datatype5'
+import './datatype5'
 // import './datatype6'
 
 let hello: string = 'Hello'
