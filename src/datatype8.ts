195d6dd9eb07a56e2d8b25eb02c739a2dff55a28
SheepHan <williamcheng0928@gmail.com>
2019-08-18 17:40:41 +0800

    add

--- src/datatype8.ts
+++ src/datatype8.ts
@@ -1,18 +1,18 @@
 //泛型：不预先确定的数据类型，具体的类型在使用的时候才能确定
 //把泛型理解为代表类型的参数
 
-function log<T>(value:T):T{
+function log12<T>(value:T):T{
   console.log(value)
   return value
 }
 
-log<string[]>(['a','b'])
-log(['a','b'])
+log12<string[]>(['a','b'])
+log12(['a','b'])
 
 
 //泛型接口
 interface Log<T>{
   (value:T):T
 }
-let myLog:Log<number>=log
+let myLog:Log<number>
 myLog(1)
