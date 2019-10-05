aaf21ef4fb22fee6e3e281ea5990b051e5328ceb
SheepHan <williamcheng0928@gmail.com>
2019-10-05 23:32:25 +0800

    重拾TypeScript

--- src/datatype1.ts
+++ src/datatype1.ts
@@ -6,21 +6,21 @@
 
   //数组
   let arr1: number[] = [1, 2, 3]
-  let arr2: Array<number> = [1, 2, 3] //数组泛型
-  let arr3: Array<number | string> = [1, 2, 3, '4'] //数组泛型 Array 是泛型接口
+  let arr2: Array<number> = [1, 2, 3] //数组泛型 Array 是泛型接口
+  let arr3: Array<number | string> = [1, 2, 3, '4'] //  | 是联合类型
 
   //元组
   let tuple: [number, string] = [1, '1'] //数组合并了相同类型的对象，而元组（Tuple）合并了不同类型的对象
-  //越界访问
-  tuple.push(2)
-  console.log(tuple) //可以越界添加
+
+  tuple.push(2)   //越界添加
+  console.log(tuple)
   // tuple[2] //但是不能越界访问
 
   //函数
   let add = (x: number, y: number): number => x + y
   let add1 = (x: number, y: number) => x + y //通常函数的返回值类型可以省略，这就利用了ts的类型推断功能
 
-  let compute: (x: number, y: number) => number
+  let compute: (x: number, y: number) => number  
   compute = (a, b) => a + b
 
   //对象
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import './datatype1' //基本类型
-import './datatype2' //枚举
+// import './datatype1' //基本类型
+// import './datatype2' //枚举
 import './datatype3' //对象类型接口 1、额外多的属性
-import './datatype3-1' //对象类型接口  2、可选属性
-import './datatype3-2' //对象类型接口  3、只读属性
-import './datatype3-3' //对象类型接口  4、可索引类型接口，当接口属性个数不确定时候使用
-import './datatype4'  //函数类型接口
-import './datatype5'  //函数定义
-import './datatype6'  //类1：继承和成员修饰
-import './datatype6-1' // 类2：抽象类与多态
-import './datatype7'   //类与接口的关系
-import './datatype8'   //泛型
-import './datatype9'   //类与泛型约束
-import './datatype10'   //类型推断
-import './datatype10-1'   //类型兼容性
-import './datatype10-2'   //类型保护
+// import './datatype3-1' //对象类型接口  2、可选属性
+// import './datatype3-2' //对象类型接口  3、只读属性
+// import './datatype3-3' //对象类型接口  4、可索引类型接口，当接口属性个数不确定时候使用
+// import './datatype4'  //函数类型接口
+// import './datatype5'  //函数定义
+// import './datatype6'  //类1：继承和成员修饰
+// import './datatype6-1' // 类2：抽象类与多态
+// import './datatype7'   //类与接口的关系
+// import './datatype8'   //泛型
+// import './datatype9'   //类与泛型约束
+// import './datatype10'   //类型推断
+// import './datatype10-1'   //类型兼容性
+// import './datatype10-2'   //类型保护
 let hello: string = 'Hello'
 document.querySelectorAll('.app')[0].innerHTML = hello
